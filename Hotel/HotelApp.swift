3b8029323faffdb6874d81ec056df2d4bae25737
Togosling <toga.abdyldaev@gmail.com>
2023-12-17 12:51:04 +0600

    Initial Commit

---
+++ Hotel/HotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelApp.swift
+//  Hotel
+//
+//  Created by Тагай Абдылдаев on 2023/12/17.
+//
+
+import SwiftUI
+
+@main
+struct HotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
